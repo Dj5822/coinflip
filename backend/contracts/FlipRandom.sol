2322c02bb56b45595c7ba53898ed31965ec91e3b
Lenoftawa <paul.ranford@gmail.com>
2025-03-18 11:16:17 +1300

    Update FlipRandom.sol
    Added boolean return value

--- backend/contracts/FlipRandom.sol
+++ backend/contracts/FlipRandom.sol
@@ -6,7 +6,7 @@ pragma solidity >=0.7.0 <0.9.0;
                          A TRIVIAL RANDOM COIN FLIP CONTRACT
 This contract has a single function that flips a coin and sends the prize depending on a random number.
 The random number is generated using a hash of several highly unpredictable valaues to minimise front-running.
-The prize is 0.01 ETH and the contract balabce cannot fall below 1 ETH. 
+The prize is 0.01 ETH and the contract balance cannot fall below 1 ETH. 
 */
 
 contract FlipRandom {
@@ -17,15 +17,16 @@ contract FlipRandom {
 
     // flipCoin() generates a best-effort front-run resistant random number.
     // Sends a prise if it is even.  
-    function flipCoin() private {
+    function flipCoin() private returns (bool){
         require(address(this).balance >= 1 ether, "Fund too low");
         // Generate a random number
         uint rand = uint(keccak256(abi.encodePacked(msg.sender, blockhash(block.number),block.gaslimit, block.prevrandao, block.timestamp)));
         bool win = (rand % 2 == 0); // The user has won if the random number is even
         if (win) { 
             sendPrize();
-        }
+         } 
         emit flip(msg.sender, win); // Log the flip
+        return (win); 
     }
 
     function sendPrize() private {
