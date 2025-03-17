2b76d8db2872342d32ee600fb4979ff88db19537
Lenoftawa <paul.ranford@gmail.com>
2025-03-18 10:20:46 +1300

    Create Flip.sol
    First commit of untested code.
    
    Usage:
    
    1. Deploy contract Flip.
    2. For now: Construct with _rand =1 and _token = 0.
    3. Call flipCoin()
    4. Check "sent" event for your address (will show true or false)
    5. Check wallet for new ETH if sent shows true.

---
+++ backend/contracts/Flip.sol
@@ -0,0 +1,85 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Flip {
+
+    bool private randomise;
+    uint256 private flipCount;  
+    address tokenAddress;
+
+    // flipstate mapping (0 == new address, 1 == odd flips, 2 == even flips).
+    mapping(address => uint8) private flipstate;
+
+    uint256 private addresscount; 
+
+    // Log successful flips
+    event flip(address, uint8); 
+
+        constructor(bool _rand, address _token) {
+        
+        randomise = _rand; // true or false depending on if randomise is required
+        tokenAddress = _token; // ERC20 token to use as prize. 0 if prize is in ETH
+    }
+
+    // flipCoin() uses a flip strategy to determine whether to send a prize or not.
+    // There are 2 strategies: alternating flips and randmomised flips.
+    function flipCoin() public {
+        require(address(this).balance >= 1 ether, "Fund too low");
+
+        // switch on type of flip
+        if (randomise == true){
+            flipRandom(); // Use randomised flips
+        } else {
+            flipAlternate(); // Use alternating flips
+        }
+
+        flipCount++; //count every flip
+
+    }
+
+    // flipAlternate() changes the flip the state for each address between 1 and 2
+    // Releases .01 ETH on every second flip
+    function flipAlternate() private {
+        // Check for second flip
+        if (flipstate[msg.sender] == 2) {
+            sendPrize();
+        }
+
+        // Log the flip
+        emit flip(msg.sender, flipstate[msg.sender]);
+
+        // Update the flip state
+        if (++flipstate[msg.sender] == 3){
+            flipstate[msg.sender] = 1; // Flip back to 1
+        } 
+
+        // Check new address
+        if (flipstate[msg.sender] == 0) {
+            addresscount++;
+        }
+    }
+
+    // flipRandom() generates a best-effort front-run resistant random number.
+    // Sends a prise if it is even.  
+    function flipRandom() private {
+      
+        if (uint(keccak256(abi.encodePacked(msg.sender, flipCount, blockhash(block.number),block.gaslimit, block.prevrandao, block.timestamp))) % 2 == 0){
+            sendPrize();
+        }
+    }
+
+    function sendPrize() private {
+        require (tokenAddress ==address(0),"ERC20 Tokens not supported yet");
+        //Send 0.01 ETH using the ugly but safe method.
+        (bool sent, bytes memory data) = msg.sender.call{
+           value: 10000000000000000
+        }("");
+        require(sent, "Failed to send Ether");
+    }
+
+    // Get the current flip state for a given address
+    function getFlipCount(address _addr) public view returns (uint256) {
+        return flipstate[_addr];
+    }
+}
