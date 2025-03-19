0c8a8855fd1479c5f3ec408c42db85e5d21d28b6
Dylan Jung <Dj5822@gmail.com>
2025-03-19 19:53:51 +1300

    Setup react router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ThemeProvider } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router";
+import "./index.css";
+import theme from "./theme.ts";
+import Homepage from "./pages/Homepage.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  </StrictMode>
+);
---
+++ frontend/src/theme.ts
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#6573C3",
+      main: "#3f51b5",
+      dark: "#0f172a",
+      contrastText: "#fff",
+    },
+    secondary: {
+      main: "#040b1c",
+    },
+  },
+});
+
+export default theme;
