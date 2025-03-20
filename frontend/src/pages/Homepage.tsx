7b236e4a6d5e82d82570bad43daaa927bbec2be6
Dylan Jung <Dj5822@gmail.com>
2025-03-20 20:43:59 +1300

    Setup flipCoin contract interaction

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -1,21 +1,41 @@
-import { Button, Stack, Typography } from "@mui/material";
+import { Button, Card, Stack, Typography, useTheme } from "@mui/material";
 import { useState } from "react";
-import { ethers } from "ethers";
+import { BaseContract, ethers } from "ethers";
+// import { IHintHelpers } from '../typechain-types';
+import abi from "../flip.json";
+import { FlipRandom } from "../types/FlipRandom";
 
 const Homepage = () => {
   const [connected, setConnected] = useState(false);
   const [walletAddress, setWalletAddress] = useState("");
+  const [contract, setContract] = useState<FlipRandom | undefined>(undefined);
+  const theme = useTheme();
+
+  const abiInterface = new ethers.Interface(abi);
+  const address = "0xF904F50409B3E95f512fBC76A1Fded76f90E26Fb";
 
   const connectWallet = async () => {
     if (window.ethereum) {
       if (!connected) {
-        // Connect the wallet using ethers.js
         const provider = new ethers.BrowserProvider(window.ethereum);
         const signer = await provider.getSigner();
+        const flipContract = new ethers.Contract(
+          address,
+          abiInterface
+        ) as BaseContract as FlipRandom;
+        const connectedFlipContract = flipContract.connect(signer);
+        setContract(connectedFlipContract);
         const _walletAddress = await signer.getAddress();
         setConnected(true);
         setWalletAddress(_walletAddress);
       }
+    }
+  };
+
+  const flipCoin = async () => {
+    if (contract) {
+      const result = await contract.flipCoin();
+      console.log(result);
     }
   };
 
@@ -28,6 +48,7 @@ const Homepage = () => {
         minWidth: "100vw",
         minHeight: "100vh",
         marginLeft: 0,
+        backgroundColor: theme.palette.background.default,
       }}
     >
       <Typography
@@ -38,7 +59,21 @@ const Homepage = () => {
         Replace with image of coin
       </Typography>
       {connected ? (
-        <Typography>Connected to {walletAddress}</Typography>
+        <Card
+          sx={{
+            padding: "72px",
+            placeItems: "center",
+          }}
+        >
+          <Typography>Connected to {walletAddress}</Typography>
+          <Button
+            variant="contained"
+            sx={{ marginTop: "48px" }}
+            onClick={flipCoin}
+          >
+            Flip Coin
+          </Button>
+        </Card>
       ) : (
         <Button variant="contained" onClick={connectWallet}>
           Connect Wallet
